41f2d358989e9ae135573e65335cd1cd9256cbed
MAR <bobbybluebland@MARs-Mac-Pro.fios-router.home>
2021-08-22 12:31:25 -0400

    UberAPP

---
+++ store.js
@@ -0,0 +1,8 @@
+import { configurationStore, configureStore } from "@reduxjs/toolkit";
+import navReducer from "./slices/navSlice";
+
+export const store = configureStore({
+  reducer: {
+    nav: navReducer,
+  },
+});
